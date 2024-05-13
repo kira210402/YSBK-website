6970d5a19ffd62b9142b8e22a79cd681837cdb0e
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-05-13 20:00:48 +0700

    setup base components and route for fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { HomePage } from "./pages/HomePage";
+import { Book } from "./pages/Book";
+import { LeaderBoard } from "./pages/LeaderBoard";
+import { Activities } from "./pages/Activities";
+import { User } from "./pages/User";
+import { NoMatch } from "./pages/NoMatch";
+
 function App() {
   return (
-    <div className="App">
-      ok
-    </div>
+    <>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/books" element={<Book />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/users" element={<User />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/pages/HomePage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./homePage.css";
+
+export const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
